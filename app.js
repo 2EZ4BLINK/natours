91da6fe71d392f5ce01defe936f6d23080b9b744
2EZ4BLINK <blinkcode24@gmail.com>
2026-06-11 21:46:49 +0800

    update: added eslint and pettier config

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
-const tourRouter = require("./routes/tourRoutes");
-const userRouter = require("./routes/userRoutes");
+const express = require('express');
+
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 // Middleware
-if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
-  console.log("Hello from middleware");
+  console.log('Hello from middleware');
   next();
 });
 app.use((req, res, next) => {
@@ -20,7 +23,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
